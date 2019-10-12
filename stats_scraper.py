efa6dca8e4d4d12c335f564004b470bea38a0ae4
Drew <ajjansen1818@gmail.com>
2019-10-12 01:16:24 -0400

    added support for scraping multiple tables

--- stats_scraper.py
+++ stats_scraper.py
@@ -5,30 +5,45 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup,Comment
 import requests
 import pandas as pd
 
 writer = pd.ExcelWriter('hockeyStats.xlsx', engine='xlsxwriter')
 
-response = requests.get("https://www.hockey-reference.com/playoffs/NHL_2019_goalies.html") 
-soup = BeautifulSoup(response.content, "html.parser")  
-    
-statsTable = soup.find("table",id="stats")
+domains = ["https://www.hockey-reference.com/"]
+subdomains = {"leagues/NHL_2020_goalies.html":"goalies",
+              "leagues/NHL_2020_skaters.html":"skaters",
+              "leagues/NHL_2020.html":"league_summary"}
+for domain in domains:  
+    for page in subdomains:
+        url = domain+page
+        print(url)
+        response = requests.get(url) 
+        soup = BeautifulSoup(response.content, "html.parser")  
+        if subdomains[page]=="league_summary":
+            subsoup = soup.find("div",id="all_stats")
+            comment = subsoup.find(text=lambda text:isinstance(text, Comment))
+            commentsoup = BeautifulSoup(comment , 'html.parser')
+            statsTable = commentsoup.find("table")
+            print(statsTable)
+        else:
+            statsTable = soup.find("table",id="stats")
+        
+        statsDf = []
+        
+        for tr in statsTable.find_all("tr"):
+            headers =[x.string for x in tr.find_all("th",scope="col")][1:]
+            cols=tr.find_all('td')
+            cols=[x.text.strip() for x in cols]
+            if len(headers) > 0:
+                statsDf.append(headers)
+            if len(cols) > 0:
+                statsDf.append(cols)
+        
+        statsDf = pd.DataFrame(statsDf)    
+        print(statsDf)
+        
+        statsDf.to_excel(writer, sheet_name=subdomains[page])
 
-statsDf = []
-
-for tr in statsTable.find_all("tr"):
-    headers =[x.string for x in tr.find_all("th",scope="col")][1:]
-    cols=tr.find_all('td')
-    cols=[x.text.strip() for x in cols]
-    if len(headers) > 0:
-        statsDf.append(headers)
-    if len(cols) > 0:
-        statsDf.append(cols)
-
-statsDf = pd.DataFrame(statsDf)    
-print(statsDf)
-
-statsDf.to_excel(writer, sheet_name='stats')
 writer.save()
